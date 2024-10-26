ae61ca2c7318fbcc7f77f3ec070f16473a360bde
Madhudharani-S-R <727723eucs113@skcet.ac.in>
2024-10-26 15:19:44 +0530

    Header.js

---
+++ REVIEW-CC1,CC2/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header style={{ padding: '20px', background: '#444', color: 'white', textAlign: 'center' }}>
+      <h1>Movie Booking App</h1>
+    </header>
+  );
+};
+
+export default Header;
